5dbd866d11b32e3cf1365ba4ba4e22d805a598bc
MasterEnderman <yanice.thinius@pm.me>
2026-05-26 00:13:08 +0200

    Update niri.nix

--- modules/nixos/programs/niri.nix
+++ modules/nixos/programs/niri.nix
@@ -7,7 +7,7 @@
 
   config = lib.mkIf config.my.nixos.niri.enable {
     programs.niri.enable               = true;
-    hardware.opengl.enable             = true;
+    hardware.graphics.enable             = true;
     programs.xwayland.enable = true;
   };
 }
