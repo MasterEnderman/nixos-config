01ee31b2eea5180e8e979a1db5b2356a49308dd5
MasterEnderman <yanice.thinius@pm.me>
2026-05-25 23:40:59 +0200

    Update ender.nix

--- users/ender.nix
+++ users/ender.nix
@@ -1,61 +1,52 @@
-{ pkgs, import-tree, ... }:
+{ pkgs, ... }:
 
 {
   users.users.ender = {
-    isNormalUser       = true;
-    extraGroups        = [ "wheel" "networkmanager" "gamemode" ];
-    shell              = pkgs.nushell;
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "gamemode" ];
+    shell = pkgs.nushell;
     hashedPasswordFile = "/persist/passwords/ender";
   };
 
   home-manager = {
-    useGlobalPkgs    = true;
-    useUserPackages  = true;
-    extraSpecialArgs = { inherit import-tree; };
-    users.ender      = { import-tree, ... }: {
-      imports = import-tree [ ./modules/home ];
-
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.ender = {
       my.home = {
         # --- shell ---
-        nushell.enable  = true;
-        foot.enable     = true;
+        nushell.enable = true;
+        foot.enable = true;
         starship.enable = true;
-
         # --- desktop ---
-        noctalia.enable  = true;
-        niri.enable      = true;
-        hyprlock.enable  = true;
-        stylix.enable    = true;
-        fuzzel.enable    = true;
+        noctalia.enable = true;
+        niri.enable = true;
+        hyprlock.enable = true;
+        stylix.enable = true;
+        fuzzel.enable = true;
         grimblast.enable = true;
-        imv.enable       = true;
-        emote.enable     = true;
-
+        imv.enable = true;
+        emote.enable = true;
         # --- tui tools ---
-        yazi.enable          = true;
-        btop.enable          = true;
-        pulsemixer.enable    = true;
-        playerctl.enable     = true;
-        lazygit.enable       = true;
+        yazi.enable = true;
+        btop.enable = true;
+        pulsemixer.enable = true;
+        playerctl.enable = true;
+        lazygit.enable = true;
         nix-search-tv.enable = true;
-
         # --- editors and development ---
-        vscodium.enable  = true;
+        vscodium.enable = true;
         jetbrains.enable = true;
-        uv.enable        = true;
-
+        uv.enable = true;
         # --- applications ---
-        ghostwriter.enable    = true;
+        ghostwriter.enable = true;
         prism-launcher.enable = true;
-
         # --- git and ssh ---
         ssh.enable = true;
         git = {
-          enable    = true;
-          userName  = "Enderman";
+          enable = true;
+          userName = "Enderman";
           userEmail = "xxmr.endermanxx@gmail.com";
         };
-
         # --- workflow ---
         nix-workflow.enable = true;
       };
