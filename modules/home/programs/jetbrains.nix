20c69bedb6966c1126ab672aa7106d02ae597151
MasterEnderman <yanice.thinius@pm.me>
2026-06-04 08:23:39 +0000

    Update jetbrains.nix

--- modules/home/programs/jetbrains.nix
+++ modules/home/programs/jetbrains.nix
@@ -7,14 +7,15 @@
   config = lib.mkIf config.my.home.jetbrains.enable {
     home.packages = with pkgs; [
       jetbrains.idea-oss
-      temurin-bin-8
-      temurin-bin-17
-      temurin-bin-21
-      temurin-bin-25
+      temurin-bin-21  # Only the default JDK goes on PATH
     ];
 
     home.sessionVariables = {
-      JAVA_HOME = "${pkgs.temurin-bin-21}";
+      JAVA_HOME    = "${pkgs.temurin-bin-21}";
+      JAVA_8_HOME  = "${pkgs.temurin-bin-8}";
+      JAVA_17_HOME = "${pkgs.temurin-bin-17}";
+      JAVA_21_HOME = "${pkgs.temurin-bin-21}";
+      JAVA_25_HOME = "${pkgs.temurin-bin-25}";
     };
   };
 }
